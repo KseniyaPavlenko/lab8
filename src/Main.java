a787b254502aa722fc450e6bb5070e99567d1277
KseniyaPavlenko <ksusha.pavlenko2002@gmail.com>
2022-10-29 12:32:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static final int MS_TIMEOUT = 100;
+    public static final int TIMEOUT = 3000;
+
+    public static void main(String[] args) throws InterruptedException {
+        JSONGetter jsonGetter = new JSONGetter();
+        JSONGetter.url = "https://breakingbadapi.com/api/quotes";
+        jsonGetter.run();
+        int msForWaiting = 0;
+        do {
+            msForWaiting += MS_TIMEOUT;
+            Thread.sleep(MS_TIMEOUT);
+        } while (msForWaiting <= TIMEOUT && jsonGetter.jsonIn == "");
+        if (msForWaiting >= TIMEOUT) {
+            System.out.println("Не удалось загрузить данные");
+        }
+
+        Phrases phrases = new Phrases();
+
+        phrases.ReadAndParseToList(jsonGetter);
+
+        System.out.println("Imported data after parsing:\n" + phrases);
+        System.out.println(phrases);
+
+        phrases.getPhrases().sort(BreakingBadPhrase.byAuthorAsc);
+        System.out.println("After sorting by author ascending:\n" + phrases);
+        phrases.getPhrases().sort(BreakingBadPhrase.byAuthorDesc);
+        System.out.println("After sorting by author descending:\n" + phrases);
+
+        phrases.getPhrases().sort(BreakingBadPhrase.byQuoteIDAsc);
+        System.out.println("After sorting by quote ID ascending:\n" + phrases);
+        phrases.getPhrases().sort(BreakingBadPhrase.byQuoteIDDesc);
+        System.out.println("After sorting by quote ID descending:\n" + phrases);
+
+        Phrases withWalterWhite = phrases.filterByAuthor("Walter White");
+        System.out.println("Walter White phrases:\n" + withWalterWhite);
+    }
+}
---
+++ src/Phrases.java
@@ -0,0 +1,76 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Phrases {
+    private ArrayList<BreakingBadPhrase> phrases;
+
+    public ArrayList<BreakingBadPhrase> getPhrases() {
+        return phrases;
+    }
+
+    public Phrases(ArrayList<BreakingBadPhrase> phrases) {
+        this.phrases = phrases;
+    }
+
+    public void setPhrases(ArrayList<BreakingBadPhrase> phrases) {
+        this.phrases = phrases;
+    }
+
+    public Phrases() {
+        phrases = new ArrayList<>();
+    }
+
+    public void add(BreakingBadPhrase bbp) {
+        this.phrases.add(bbp);
+    }
+
+    public Phrases filterByAuthor(String str) {
+        Phrases tempObj = new Phrases();
+        for (BreakingBadPhrase phrase : this.phrases) {
+            if (phrase.getAuthor().toLowerCase().contains(str.toLowerCase()))
+                tempObj.add(phrase);
+        }
+        return tempObj;
+    }
+
+    public void ReadAndParseToList(JSONGetter jsonGetter) {
+        System.out.println("Waiting for data...");
+        String jsonString = jsonGetter.jsonIn;
+        System.out.println(jsonString);
+
+        Object obj = null;
+        try {
+            obj = new JSONParser().parse(jsonString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+
+        JSONArray jsonArray = (JSONArray) obj;
+        System.out.println();
+
+        for (Object jsonObject : jsonArray) {
+            JSONObject current = (JSONObject) jsonObject;
+            int quoteId = Integer.parseInt(String.valueOf(current.get("quote_id")));
+            String quote = (String) current.get("quote");
+            String author = (String) current.get("author");
+            String series = (String) current.get("series");
+            BreakingBadPhrase bbp = new BreakingBadPhrase(quoteId, quote, author, series);
+            phrases.add(bbp);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "Phrases (" + (new Date()).toLocaleString() + ") " + System.lineSeparator();
+        for (BreakingBadPhrase bbp : phrases) {
+            result += bbp + System.lineSeparator();
+        }
+        return result;
+    }
+}
